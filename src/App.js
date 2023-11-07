888b7f6f95671bfaa99820e23be0ca40955fae65
Muhammad Fernandes <132972538+NdeZzz05@users.noreply.github.com>
2023-11-08 04:23:32 +0700

    episode 4

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import special from "./assets/navbar/special.svg";
 import contact from "./assets/navbar/contact.svg";
 import more from "./assets/navbar/more.svg";
 import heroImage from "./assets/heroImage2.png";
+// icon main
 import bag from "./assets/iconMain/bag.svg";
 import play from "./assets/iconMain/play.svg";
+// logo company
+import airbnb from "./assets/logoCompany/airbnb.svg";
+import booking from "./assets/logoCompany/booking.svg";
+import expedia from "./assets/logoCompany/expedia.svg";
+import orbitz from "./assets/logoCompany/orbitz.svg";
+import tripadvisor from "./assets/logoCompany/tripadvisor.svg";
+// icon service
+import guide from "./assets/iconService/guide.png";
+import bookingservice from "./assets/iconService/booking.png";
+import weather from "./assets/iconService/weather.png";
 
 export const App = () => {
   const [dataToggle, setDataToggle] = useState(false);
@@ -23,7 +34,7 @@ export const App = () => {
   return (
     <>
       {/* Navigation bar */}
-      <nav className="py-9 px-4">
+      <nav className="py-4 px-4 lg:px-8">
         <div className="container mx-auto">
           <div className="flex justify-between items-center">
             <img src={logo} alt="" className="order-1 sm:order-2" />
@@ -51,7 +62,7 @@ export const App = () => {
           </div>
         </div>
         {dataLogo ? (
-          <div className="fixed bottom-0 w-full right-0 left-0 p-4 bg-white border lg:hidden z-50">
+          <div className="fixed bottom-0 w-full right-0 left-0 p-2 bg-white border lg:hidden z-50">
             <ul className="flex justify-between">
               <li>
                 <button className="flex justify-center  items-center flex-col gap-1">
@@ -98,7 +109,7 @@ export const App = () => {
 
       {/* Hero section */}
       <section>
-        <div className="container mx-auto h-2 px-4 py-4 lg:py-4">
+        <div className="container mx-auto py-4 px-4 lg:px-8">
           <div className="grid grid-cols-12 items-center">
             <div className="col-span-12 lg:col-span-4 order-2 lg:order1">
               <div>
@@ -122,6 +133,66 @@ export const App = () => {
             <div className="col-span-12 lg:col-span-8 order-1 lg:order-2">
               <div>
                 <img src={heroImage} alt="" className="w-full" />
+              </div>
+            </div>
+            <div className="col-span-12 order-3 py-8">
+              <div className="flex justify-center lg:justify-between gap-6 md:gap-7 flex-wrap">
+                <div>
+                  <img src={airbnb} alt="" className="h-5 md:h-8" />
+                </div>
+                <div>
+                  <img src={booking} alt="" className="h-5 md:h-8" />
+                </div>
+                <div>
+                  <img src={expedia} alt="" className="h-5 md:h-8" />
+                </div>
+                <div>
+                  <img src={orbitz} alt="" className="h-5 md:h-8" />
+                </div>
+                <div>
+                  <img src={tripadvisor} alt="" className="h-5 md:h-8" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Sercive section */}
+      <section>
+        <div className="container mx-auto py-4 px-4 lg:px-8 lg:py-16 overflow-x-hidden">
+          <div className="grid grid-cols-12 items-center px-2">
+            <div className="lg:col-span-4 col-span-12 flex flex-col gap-4 lg:pr-2">
+              <span className="text-pink-0 font-bold leading-tight text-center lg:text-start text-base sm:text-2xl">SERVICES</span>
+              <h1 className="font-bold sm:text-5xl text-3xl leading-tight text-grey-0 text-center lg:text-start">Our top value categories for you</h1>
+            </div>
+            <div className="lg:col-span-8 col-span-12 flex flex-col sm:flex-row gap-5 lg:w-screen md:w-auto sm:mt-16 mt-8 lg:mt-0">
+              <div className="flex flex-col justify-center sm:gap-16 gap-8 items-center w-full sm:w-[350px] rounded-[32px] border lg:p-16 sm:p-8 p-6 bg-white drop-shadow-lg">
+                <div className="">
+                  <img src={guide} alt="" />
+                </div>
+                <div className="flex flex-col sm:gap-8 gap-2">
+                  <h2 className="text-grey-0 font-bold text-lg text-center">Best Tour Guide</h2>
+                  <p className="font-inter text-lg leading-relaxed text-grey-0 opacity-50 text-center">What looked like a small patch of purple grass, above five feet.</p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center sm:gap-16 gap-8 items-center w-full sm:w-[350px] rounded-[32px] border lg:p-16 sm:p-8 p-6  bg-white drop-shadow-lg ">
+                <div className="">
+                  <img src={bookingservice} alt="" />
+                </div>
+                <div className="flex flex-col sm:gap-8 gap-2">
+                  <h2 className="text-grey-0 font-bold text-lg text-center">Easy Booking</h2>
+                  <p className="font-inter text-lg leading-relaxed text-grey-0 opacity-50 text-center">Square, was moving across the sand in their direction.</p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center sm:gap-16 gap-8  items-center w-full sm:w-[350px] rounded-[32px] border lg:p-16 sm:p-8 p-6 bg-white drop-shadow-lg">
+                <div className="">
+                  <img src={weather} alt="" />
+                </div>
+                <div className="flex flex-col sm:gap-8 gap-2">
+                  <h2 className="text-grey-0 font-bold text-lg text-center">Weather Forecast</h2>
+                  <p className="font-inter text-lg leading-relaxed text-grey-0 opacity-50 text-center">What looked like a small patch of purple grass, above five feet.</p>
+                </div>
               </div>
             </div>
           </div>
